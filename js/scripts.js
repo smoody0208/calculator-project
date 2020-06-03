cca531ae10de62b75b903bd11358a84ddd14d297
Spencer Moody <spencer.moody@outlook.com>
2020-06-03 13:33:13 -0700

    Add form and UI
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,3 @@
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-alert(multiply(number1, number2));
-
 const multiply = function(number1, number2) {
 	return number1 * number2;
 };
@@ -18,7 +14,12 @@ const subtract = function(number1, number2) {
   return number1 - number2;
 };
 
-
+$(document).ready(function() {
+  const number1 = parseInt(prompt("Enter a number:"));
+  const number2 = parseInt(prompt("Enter another number:"));
+  
+  alert(add(number1, number2));
+});
 
 
 
