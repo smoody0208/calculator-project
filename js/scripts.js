6420e27138be55501d322d09e21726a8d899a595
Spencer Moody <spencer.moody@outlook.com>
2020-06-03 14:05:56 -0700

    Add forms/UI for subtract, multiply, and divide
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -15,10 +15,43 @@ const subtract = function(number1, number2) {
 };
 
 $(document).ready(function() {
-  const number1 = parseInt(prompt("Enter a number:"));
-  const number2 = parseInt(prompt("Enter another number:"));
-  
-  alert(add(number1, number2));
+  $("form#add").submit(function(event){
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+
+$(document).ready(function() {
+  $("form#subtract").submit(function(event){
+    event.preventDefault();
+    const number1 = parseInt($("#subtract1").val());
+    const number2 = parseInt($("#subtract2").val());
+    const result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#divide").submit(function(event){
+    event.preventDefault();
+    const number1 = parseInt($("#divide1").val());
+    const number2 = parseInt($("#divide2").val());
+    const result = divide(number1, number2);
+    $("#output").text(result);
+  });
+});
+
+$(document).ready(function() {
+  $("form#multiply").submit(function(event){
+    event.preventDefault();
+    const number1 = parseInt($("#multiply1").val());
+    const number2 = parseInt($("#multiply2").val());
+    const result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+});
 });
 
 
